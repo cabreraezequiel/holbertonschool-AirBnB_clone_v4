d7244305f15ca9edbef0d6fa781a355ee4ae7263
hellerdejanuar <hellerdejanuar@gmail.com>
2022-09-14 21:22:37 +0000

    JS script almost working

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,16 @@
-const jquery = require('jquery');
-let checked_amenities = [];
+let checkedAmenities = {};
 $(document).ready(function() {
-  if ($('checkbox').is('checked')) {
-    console.log("true");
-    checked_amenities.push(amenity.id);
-  }
-  if ($('checkbox').is('checked')) {
-    checked_amenities.pop(amenity.id);
-  }
+  $('.checkbox').on('input', function() {
+    if ($(this).prop('checked')) {
+      console.log("true");
+	  checkedAmenities[($(this).data('id'))] = ($(this).data('name'));
+      //checked_amenities.push(amenity.id);
+    }
+    if (!$(this).prop('checked')) {
+      //checked_amenities.pop(amenity.id);
+      console.log("false");
+	  delete checkedAmenities[($(this).data('id'))];
+	}
+    $(".amenities h4").text(Object.values(checkedAmenities).toString(", ")); // MISSING CORRECT DELIMITER
+  });
 });
