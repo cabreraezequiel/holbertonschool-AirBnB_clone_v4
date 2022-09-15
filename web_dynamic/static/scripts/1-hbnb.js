91c849e15347009712cd1eecefddaf21900f7f3e
Ezequiel Cabrera <98030236+cabreraezequiel@users.noreply.github.com>
2022-09-15 13:39:40 -0300

    Update 1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -4,13 +4,11 @@ $(document).ready(function() {
     if ($(this).prop('checked')) {
       console.log("true");
 	  checkedAmenities[($(this).data('id'))] = ($(this).data('name'));
-      //checked_amenities.push(amenity.id);
     }
     if (!$(this).prop('checked')) {
-      //checked_amenities.pop(amenity.id);
       console.log("false");
 	  delete checkedAmenities[($(this).data('id'))];
 	}
-    $(".amenities h4").text(Object.values(checkedAmenities).toString(", ")); // MISSING CORRECT DELIMITER
+    $(".amenities h4").text(Object.values(checkedAmenities).join(', '));
   });
 });
